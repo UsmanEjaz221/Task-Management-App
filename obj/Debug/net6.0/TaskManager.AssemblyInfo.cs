74f0a8f2f83e613adae87b33be11fb2a4b2df8ab
RaoUmair55 <raoumair554@gmail.com>
2025-01-01 00:30:01 +0500

    Color adjustment

--- obj/Debug/net6.0/TaskManager.AssemblyInfo.cs
+++ obj/Debug/net6.0/TaskManager.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d26e23ba3c131677e494bc638f34ae90cdaeef97")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b915cadaf11f6d182a6d3ffc1818f8ef9ed7a563")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
